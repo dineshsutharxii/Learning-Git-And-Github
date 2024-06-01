7b55894d258b5babeabbc2e827fc55d942a3c128
dinesh <dineshsutharxii@gmail.com>
2024-06-01 11:15:24 +0530

    commiting sumoftwonumbers files changes

---
+++ sumoftwonumber.java
@@ -0,0 +1,11 @@
+/**
+ * sumoftwonumber
+ */
+public class sumoftwonumber {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 13;
+        System.out.println(a+b);
+    }
+}

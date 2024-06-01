14f6d01d3123d553971a74b312d7fbd30506365b
dinesh <dineshsutharxii@gmail.com>
2024-06-01 12:06:11 +0530

    commiting multiply and difference file changes

---
+++ multipleoftwonumbers.java
@@ -0,0 +1,8 @@
+public class multipleoftwonumbers {
+    
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 13;
+        System.out.println("Multiple of two numbers : " + (a*b));
+    }
+}

329e6c55de4fa373a9d98475030b90127a7c0fac
dinesh <dineshsutharxii@gmail.com>
2024-06-01 12:29:30 +0530

    Changes in Multiply file

--- diffoftwonumbers.java
+++ diffoftwonumbers.java
@@ -3,6 +3,6 @@ public class diffoftwonumbers {
     public static void main(String[] args) {
         int a = 10;
         int b = 13;
-        System.out.println("Differnce of two numbers : " + (a-b));
+        System.out.println("Differnce of two numbers is : " + (a-b));
     }
 }

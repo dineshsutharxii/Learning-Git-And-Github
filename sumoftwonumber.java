2534faf5947a81505104f5f4dc6ebc6425771281
dinesh <dineshsutharxii@gmail.com>
2024-06-01 11:32:35 +0530

    adding sumoftwonumber

--- sumoftwonumber.java
+++ sumoftwonumber.java
@@ -6,6 +6,6 @@ public class sumoftwonumber {
     public static void main(String[] args) {
         int a = 10;
         int b = 13;
-        System.out.println(a+b);
+        System.out.println("Sum of two numbers : " + (a+b));
     }
 }
